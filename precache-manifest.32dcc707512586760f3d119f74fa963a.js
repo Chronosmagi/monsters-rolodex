fbe9d51feef42b3cd0e2923e475d35cc5d17647a
ChronoMagi <chronomagi72@gmail.com>
2019-08-05 19:07:04 -0400

    Updates

---
+++ precache-manifest.32dcc707512586760f3d119f74fa963a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80135bf3df515664fc8ebb886d8fa10f",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "43d5b585fba319165eb5",
+    "url": "/monsters-rolodex/static/css/main.5ba179b8.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "43d5b585fba319165eb5",
+    "url": "/monsters-rolodex/static/js/main.7006fe2d.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
